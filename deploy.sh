1a0a13a36a5d2f303f3f0136ddf044afb7b07aa0
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-12-05 17:26:13 +0530

    add docgen to same ghpages

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 TMP=/tmp/tldiff
 rm -rf "$TMP"
@@ -6,6 +7,22 @@ cp diff.js atom.xml diffs.js diff.html diff.css "$TMP"
 git checkout --orphan gh-pages
 mv "$TMP"/* .
 git add diff.js atom.xml diffs.js diff.html diff.css -f
-git commit -am "Deploy new layer"
+# the below lines were copied from
+# https://github.com/LonamiWebs/Telethon/blob/master/update-docs.sh
+git commit -am "TLDiffer: Deploy new layer"
+curpath=$(pwd)
+git clone https://github.com/LonamiWebs/Telethon /tmp/docgen/
+cd /tmp/docgen/
+python setup.py gen docs
+rm -rf /tmp/docs
+mv docs/ /tmp/docs
+cd ${curpath}
+mkdir -p docs
+cd docs
+# there's probably better ways but we know none has spaces
+rm -rf $(ls /tmp/docs)
+mv /tmp/docs/* .
+git add constructors/ types/ methods/ index.html js/search.js css/ img/
+git commit --amend -m "DocGen: Update documentation"
 git push --force -u origin gh-pages
 git checkout master
