9e1463d9afc6c9be7488d2eab46c5d246d7ce36d
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-12-05 19:50:51 +0530

    fix layer link, maybe (in future) figure out how to gen docs, without cloning entire Telethon

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
+IFS="
+"
 TMP=/tmp/tldiff
 rm -rf "$TMP"
 mkdir "$TMP"
 cp diff.js atom.xml diffs.js diff.html diff.css "$TMP"
+git branch -D gh-pages
 git checkout --orphan gh-pages
 mv "$TMP"/* .
+ls
 git config --global user.email "johnprestonmail@gmail.com"
 git config --global user.name "GitHub Action <John Preston>"
 git add diff.js atom.xml diffs.js diff.html diff.css -f
@@ -16,14 +20,15 @@ curpath=$(pwd)
 git clone https://github.com/LonamiWebs/Telethon /tmp/docgen/
 cd /tmp/docgen/
 rm telethon_generator/data/api.tl
-wget "https://raw.githubusercontent.com/SpEcHiDe/Telethon/f2a366c70e21ca02ea612e3f82fd1b6462bb8005/telethon_generator/data/api.tl" -O telethon_generator/data/api.tl
+cimmot="446a06ba461d82aa06aeab23bf92524095b3dd76"
+# cimmot="main"
+wget "https://github.com/SpEcHiDe/telegram-bot-api-spec/raw/${cimmot}/mtproto.tl" -O telethon_generator/data/api.tl
 python setup.py gen docs
 rm -rf /tmp/docs
 mv docs/ /tmp/docs
 cd ${curpath}
 mkdir -p docs
 cd docs
-# there's probably better ways but we know none has spaces
 rm -rf $(ls /tmp/docs)
 mv /tmp/docs/* .
 git config --global user.email "totufals@hotmail.com"
