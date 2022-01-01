79f67a605301ac7b8b43746af26ee795a86b5881
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-01-01 17:48:54 +0530

    attempt 72

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,8 @@ git config --global user.email "johnprestonmail@gmail.com"
 git config --global user.name "GitHub Action <John Preston>"
 cimmot="64d108c6160716bdf999e98f78a8d50117ba88a0"
 wget "https://github.com/SpEcHiDe/telegram-bot-api-spec/raw/${cimmot}/mtproto.tl" -O schemes/${cimmot}.tl
-git add schemes/ diff.js atom.xml diffs.js diff.html diff.css -f
+rm README.md deploy.sh get_all_tl.py
+git add schemes/ diff.js atom.xml diffs.js diff.html diff.css README.md deploy.sh get_all_tl.py -Af
 # the below lines were copied from
 # https://github.com/LonamiWebs/Telethon/blob/master/update-docs.sh
 git commit -am "TLDiffer: Deploy new layer"
@@ -37,7 +38,7 @@ mv /tmp/docs/* .
 git config --global user.email "totufals@hotmail.com"
 git config --global user.name "GitHub Action <Lonami Exo>"
 git add constructors/ types/ methods/ index.html js/search.js css/ img/
-git commit --amend -m "DocGen: Update documentation"
+git commit -m "DocGen: Update documentation"
 rm -rf /tmp/docgen
 
 git clone https://github.com/pyrogram/pyrogram /tmp/docgen/
@@ -59,8 +60,9 @@ cd pyrogram
 mv /tmp/pydocs/* .
 git config --global user.name "GitHub Action <Dan>"
 git config --global user.email "14043624+delivrance@users.noreply.github.com"
+touch .nojekyll  # https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
 git add . -A
-git commit --amend -m "DocGen: Update documentation"
+git commit -m "DocGen: Update documentation"
 rm -rf /tmp/docgen
 
 git push --force -u origin gh-pages
