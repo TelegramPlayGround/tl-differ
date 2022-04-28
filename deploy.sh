80c0f3d1fc091cea6b31ca8cd8bd8ee5f6e364e5
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-04-28 22:29:17 +0530

    testings

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git config --global user.email "johnprestonmail@gmail.com"
 git config --global user.name "GitHub Action <John Preston>"
 wget "https://github.com/TelegramPlayGround/tg-api-spec-s/raw/data/tdesktop.tl" -O schemes/latest.tl
 
-git add schemes/ diff.js atom.xml diffs.js diff.html index.html diff.css README.md deploy.sh get_all_tl.py -Af
+git add schemes/ diff.js atom.xml diffs.js diff.html index.html diff.css -Af
 # the below lines were copied from
 # https://github.com/LonamiWebs/Telethon/blob/master/update-docs.sh
 git commit -am "${current_date} TLDiffer: Deploy new layer"
@@ -37,8 +37,7 @@ cd ${curpath}
 mkdir -p pyrogram
 cd pyrogram
 mv /tmp/pydocs/* .
-touch ../.nojekyll  # https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
-git add ../.nojekyll
+
 git config --global user.name "GitHub Action <Dan>"
 git config --global user.email "14043624+delivrance@users.noreply.github.com"
 git add . -A
