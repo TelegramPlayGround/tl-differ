bb25d5170b36417fdb58c76a8bd4849fec89648b
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-05-14 18:50:49 +0530

    remove typo

--- deploy.sh
+++ deploy.sh
@@ -64,7 +64,6 @@ git config --global user.email "totufals@hotmail.com"
 git config --global user.name "GitHub Action <Lonami Exo>"
 git add constructors/ types/ methods/ index.html js/search.js css/ img/
 cd ..
-rm -rf ../tdesktop/ > /dev/null 2>&1
 git add . -A
 git status
 git commit -m "${current_date} DocGen: Update TeLethoN documentation" > /dev/null 2>&1
